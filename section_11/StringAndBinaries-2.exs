a42ab868eca312daf7b2775d36f76d36d1cfa135
KID the Euforia <kid0725+github@gmail.com>
2019-02-14 16:12:41 +0900

    update StringAndBinaries-2
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- section_11/StringAndBinaries-2.exs
+++ section_11/StringAndBinaries-2.exs
@@ -1,9 +1,7 @@
 defmodule MyParse do
   def anagram?(word1, word2) do
-    sum1 = 0
-    sum2 = 0
-    for char1 <- word1, do: sum1 = sum1 + char1
-    for char2 <- word2, do: sum2 = sum2 + char2
+    sum1 = Enum.sum(for char1 <- word1, do: char1)
+    sum2 = Enum.sum(for char2 <- word2, do: char2)
     sum1 == sum2
   end
 end
